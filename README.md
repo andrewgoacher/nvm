349c20bed5a8f597b7c2c41a3324a61a6445629e
Andrew Goacher <andrewgoacher@outlook.com>
2022-07-11 22:51:12 +0100

    Add text describing new idea and add additional supporting lib

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # nvm
 
-A wrapper for node that allows for use without admin and will utilise the nvmrc file.
+A wrapper around node and related commands that allows for full use without admin and will properly utilise an nvmrc file.
+Will allow multiple versions of node to run simultaneously.
 
-nvm commands:
+## commands:
 
-* all - Lists all versions from the last 5 years in order
-* all-verbose - List all versions
-* install - installs a version
-* use - sets a version as default - can be used with install
-* uninstall - removes a version
-
-Currently windows only.  Because I don't want to deal with cross platform
-
-npm, npx  and node projects basically mirror their counter parts
+* list
+* use
+* install
+* uinstall
+* run
+* init
--- nvm/Program.cs
+++ nvm/Program.cs
@@ -8,6 +8,59 @@ using System.CommandLine;
 // lts: gallium = latest-gallium/
 // current is first
 
+/**
+ * This will be the front runner info for nvm.  
+ * Each task that this program can perform will be listed here with most interactions.
+ * Each task will be represented as a specific application service responding to a command
+ * Tasks should come with accompanying tests to ensure behavior is correct.
+ * 
+ * init:
+ * Initializes the tool.  The first time nvm is used it should run this regardless.
+ * the output of this command will be a settings file stored in app data for the tool
+ * Init will be able to determine where the node versions are installed and add this to the path
+ * 
+ * Default will put all data in appsettings 
+ * Options to specify otherwise (use tab complete if possible here to find the path)
+ * 
+ * install:
+ * 
+ * Will install a specific version into the designated location.
+ * Optional command arg to set this version as the "current"
+ * Current means the version of node used when no specific version is provided
+ * or an nvmrc file isn't used.
+ * not providing a version will provide a prompt
+ * 
+ * uninstall: 
+ * Will remove a version from the system
+ * if this version if current then prompt to choose new default if any are available will be provided
+ * 
+ * not providing a version will show prompt
+ * 
+ * use: 
+ * Sets the version as current
+ * if the version doesn't exist get the choice to install it or set a different one
+ * Not providing a version will provide a prompt
+ * 
+ * list:
+ * Will show all locally installed versions
+ * optional argument to show 12 months of recent versions
+ * 
+ * run;
+ * 
+ * Will run the provided command in the specified verison
+ * 
+ * nvm run node -v -> runs "node -v" against currently set version OR against nvmrc version
+ * nvm run node -v --12.13.14 will run "node -v" against the installed 12.13.14 version
+ * 
+ * not all versions might have a tool.
+ * 
+ * Special verions
+ * 
+ * latest/
+ * lts/
+ * 
+ */
+
 namespace nvm
 {
     public static class Program
