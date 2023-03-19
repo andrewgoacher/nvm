397ab5342be3cb51b2f70c57df88cd0601ea214f
Andrew Goacher <andrewgoacher@outlook.com>
2023-03-19 21:08:56 +0000

    Add Options and option handlers for install and use

---
+++ src/nvm/Configuration/InstallOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace nvm.Configuration;
+
+[Verb("install", HelpText = "Install the specified node version")]
+internal class InstallOptions
+{
+    [Value(0, Required = true)]
+    public string Version { get; }
+}
---
+++ src/nvm/Program.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using nvm.Configuration;
+
+namespace nvm;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Parser.Default
+            .ParseArguments<InstallOptions, UseOptions>(args)
+            .WithParsed<InstallOptions>(HandleInstall)
+            .WithParsed<UseOptions>(HandleUse);
+    }
+
+    private static void HandleInstall(InstallOptions options)
+    {
+
+    }
+
+    private static void HandleUse(UseOptions options)
+    {
+
+    }
+}
